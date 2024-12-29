a7cee3d9c6c621f82b1cb06b4a1dd8282a6d9c5a
William <botika1514@gmail.com>
2024-12-28 23:48:52 -0800

    Added django-bootstrap4 to requirements

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -17,7 +17,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = '0k7(=3xy503l5nio^)k29b1ryxyfm)r%k)))4xqq3li4)og%9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
